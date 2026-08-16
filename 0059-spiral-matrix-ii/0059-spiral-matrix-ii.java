520e9988585fb6051286ec8e11200c3d1851e717
Shravan-009 <shravan.r.chavan01@gmail.com>
2026-08-16 13:34:08 +0530

    Time: 0 ms (100%), Space: 42.4 MB (97.93%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] arr = new int[n][n];
+        int maxrow=n-1;
+        int minrow=0;
+        int maxcol=n-1;
+        int mincol=0;
+        int count=1;
+
+        while(count<=n*n){
+            for(int c = mincol;c<=maxcol;c++){
+                arr[minrow][c]=count;
+                count++;
+            }
+            for(int r  =minrow+1;r<=maxrow;r++){
+                arr[r][maxcol]=count;
+                count++;
+            }
+            for(int c = maxcol-1;c>=mincol;c--){
+                arr[maxrow][c]=count;
+                count++;
+            }
+            for(int r = maxrow-1;r>=minrow+1;r--){
+                arr[r][mincol]=count;
+                count++;
+            }
+            mincol++;
+            maxcol--;
+            minrow++;
+            maxrow--;
+        }
+        return arr;
+    }
+}
